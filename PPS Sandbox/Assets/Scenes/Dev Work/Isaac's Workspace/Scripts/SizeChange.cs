27ba539b3490baf478b669528fa50cc092e2ae99
Isaac Turpin <Isaac2.Turpin@live.uwe.ac.uk>
2023-11-14 15:50:35 +0000

    Cleaning up SizeChange script
    deleted commented out old code

--- PPS Sandbox/Assets/Scenes/Dev Work/Isaac's Workspace/Scripts/SizeChange.cs
+++ PPS Sandbox/Assets/Scenes/Dev Work/Isaac's Workspace/Scripts/SizeChange.cs
@@ -9,65 +9,38 @@ public class SizeChange : MonoBehaviour
 
     private bool shrunk = false;
 
-    public void ChangeSize(AmmoType ammoType /*, float changeAmount */)
+    public void ChangeSize(AmmoType ammoType)
     {
         if(ammoType.ToString() == "Shrink")
         {
-            ShrinkObject(/*changeAmount*/);
+            ShrinkObject();
             
         }
         if(ammoType.ToString() == "Grow")
         {
-            GrowObject(/*changeAmount*/);
+            GrowObject();
         }
     }
 
-    void ShrinkObject(/*float changeAmount*/)
+    void ShrinkObject()
     {
-        //Debug.Log("working shrink " + changeAmount);
         Vector3 currentSize = GetComponent<Transform>().localScale;
 
         if(currentSize != smallestSize)
         {
             gameObject.transform.localScale = smallestSize;
             shrunk= true;
-
-            /*
-            Vector3 newSize = gameObject.transform.localScale -= new Vector3(changeAmount, changeAmount, changeAmount);
-
-            if(newSize != smallestSize) 
-            {
-                gameObject.transform.localScale = newSize;
-            }
-            else
-            {
-                return;
-            }
-            */
         }
     }
 
-    void GrowObject(/*float changeAmount*/)
+    void GrowObject()
     {
-        //Debug.Log("working increase " + changeAmount);
         Vector3 currentSize = GetComponent<Transform>().localScale;
 
         if (currentSize != maxSize)
         {
-            //Vector3 newSize = gameObject.transform.localScale += new Vector3(changeAmount, changeAmount, changeAmount);
-
             gameObject.transform.localScale = maxSize;
             shrunk = false;
-
-            /*if (newSize != maxSize)
-            {
-                gameObject.transform.localScale = newSize;
-            }
-            else
-            {
-                return;
-            }
-            */
         }
     }
 
@@ -75,34 +48,4 @@ public class SizeChange : MonoBehaviour
     {
         return shrunk;
     }
-
-     /*
-    private void Update()
-    {
-        CheckSmallest();
-        CheckLargest();
-    }
-
-    void CheckSmallest()
-    {
-        if (gameObject.transform.localScale.x < smallestSize.x
-            && gameObject.transform.localScale.y < smallestSize.y
-            && gameObject.transform.localScale.z < smallestSize.z)
-        {
-            gameObject.transform.localScale = smallestSize;
-            //Debug.Log("too small");
-        }
-    }
-
-    void CheckLargest()
-    {
-        if (gameObject.transform.localScale.x > maxSize.x
-            && gameObject.transform.localScale.y > maxSize.y
-            && gameObject.transform.localScale.z > maxSize.z)
-        {
-            gameObject.transform.localScale = maxSize;
-            //Debug.Log("too big");
-        }
-    }
-     */
 }
