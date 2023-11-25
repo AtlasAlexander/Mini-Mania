4da43ba408d42a35c1ebf29d58c917b501481ae0
Marcus Ochai <marcus2.ochai@live.uwe.ac.uk>
2023-11-25 23:04:26 +0000

    seesaw works

--- PPS Sandbox/Assets/Scenes/Dev Work/Marcus's Workspace/Scripts/Ruler_left_script.cs
+++ PPS Sandbox/Assets/Scenes/Dev Work/Marcus's Workspace/Scripts/Ruler_left_script.cs
@@ -2,23 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ruler_right_script : MonoBehaviour
+public class Ruler_left_script : MonoBehaviour
 {
     public GameObject ruler;
-    public GameObject LeftSide;
-    //public GameObject player;
-    public bool right_side_down;
+    public GameObject RightSide;
+    public bool left_side_down;
 
     void Update()
     {
-        if (right_side_down == true)
+        if (left_side_down == true)
         {
-            ruler.transform.Rotate(50 * Time.deltaTime,0,0);
+            ruler.transform.Rotate(-30 * Time.deltaTime,0,0);
         }
-        // if (ruler.transform.localRotation.eulerAngles.x = 27f)
-        // {
-        //     right_side_down = false;
-        // }
     }
 
     public void OnTriggerEnter(Collider other)
@@ -26,13 +21,13 @@ public class Ruler_right_script : MonoBehaviour
         if (other.gameObject.CompareTag("Player"))
         {
             //this.gameObject.GetComponent<BoxCollider>().enabled = false;
-            right_side_down = true;
+            left_side_down = true;
         }
         if (other.gameObject.CompareTag("Floor"))
         {
-            right_side_down = false;
+            left_side_down = false;
             this.gameObject.GetComponent<BoxCollider>().enabled = false;
-            LeftSide.GetComponent<BoxCollider>().enabled = true;
+            RightSide.GetComponent<BoxCollider>().enabled = true;
         }
     }
 }
