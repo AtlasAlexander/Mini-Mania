022679e1f9e06b2f2a0ba7cd7024442f0695cc4e
Jake <jake2.webster@live.uwe.ac.uk>
2023-12-02 17:12:49 +0000

    Fixed weight bug

--- PPS Sandbox/Assets/Scenes/Dev Work/Jake's Workspace/Scripts/Stats.cs
+++ PPS Sandbox/Assets/Scenes/Dev Work/Jake's Workspace/Scripts/Stats.cs
@@ -5,4 +5,25 @@ using UnityEngine;
 public class Stats : MonoBehaviour
 {
     public float Weight = 100;
+    float DefaultWeight = 100;
+
+    private void Awake()
+    {
+        DefaultWeight = Weight;
+        if(GetComponent<SizeChange>().startSmall)
+        {
+            DefaultWeight = Weight * 5;
+        }
+    }
+    private void LateUpdate()
+    {
+        if(Weight < DefaultWeight/5)
+        {
+            Weight = DefaultWeight / 5;
+        }
+        if(Weight > DefaultWeight)
+        {
+            Weight = DefaultWeight;
+        }
+    }
 }
