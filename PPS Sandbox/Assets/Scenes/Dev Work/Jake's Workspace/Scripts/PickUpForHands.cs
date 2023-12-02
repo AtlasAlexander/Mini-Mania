ebdf9325fcb1efbd026b71411908e935146ed81f
Jake <jake2.webster@live.uwe.ac.uk>
2023-12-02 04:34:20 +0000

    New Carry scripts!!
    got too messy with "big obj small obj", needed an overhaul due to bugs, so did the overhaul

---
+++ PPS Sandbox/Assets/Scenes/Dev Work/Jake's Workspace/Scripts/PickUpForHands.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpForHands : MonoBehaviour
+{
+    [SerializeField] Camera FPCamera;
+    [SerializeField] float pickUpRange = 3f;
+    public float outOfLosDist = 4f;
+
+    GameObject Player;
+    public PlayerControls playerControls;
+
+    public bool carrying;
+
+    private void OnEnable()
+    {
+        playerControls.Enable();
+    }
+
+    private void Awake()
+    {
+        FPCamera = Camera.main;
+        Player = GameObject.FindGameObjectWithTag("Player");
+        playerControls = new PlayerControls();
+    }
+
+    private void Update()
+    {
+        //check for input
+        playerControls.Movement.Interact.performed += x => IsCarrying();
+    }
+
+    void IsCarrying()
+    {
+        //toggle
+        carrying = !carrying;
+        //raycast
+        if(carrying)
+        {
+            ProcessRaycast();
+        }
+    }
+
+    void ProcessRaycast()
+    {
+        //send raycast
+        RaycastHit hit;
+        //if there is an object in range
+        if (Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, pickUpRange))
+        {
+            //checks for pick up script on obj
+            PickUpForObj carry = hit.transform.GetComponent<PickUpForObj>();
+            if(carry != null)
+            {
+                carry.InHand = true;
+            }
+            else
+            {
+                carrying = false;
+            }
+        }
+        //if there is not an object in range
+        else
+        {
+            carrying = false;
+        }
+    }
+}
---
+++ PPS Sandbox/Assets/Scenes/Dev Work/Jake's Workspace/Scripts/PickUpForObj.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpForObj : MonoBehaviour
+{
+    public float followSpeed;
+    private float defaultSpeed;
+
+    public bool InHand;
+
+    public GameObject hands;
+
+    private void Awake()
+    {
+        hands = GameObject.FindGameObjectWithTag("Hands");
+        defaultSpeed = followSpeed;
+    }
+
+    private void Update()
+    {
+        if(InHand)
+        {
+            ObjInHand();
+        }
+        else
+        {
+            gameObject.GetComponent<Rigidbody>().useGravity = true;
+        }
+    }
+
+    private void ObjInHand()
+    {
+        if(!hands.GetComponent<PickUpForHands>().carrying)
+        {
+            InHand = false;
+        }
+        //adds flow to carrying objects
+        var step = followSpeed * Time.deltaTime; // calculate distance to move
+        transform.position = Vector3.MoveTowards(transform.position, hands.transform.position, step);
+        gameObject.GetComponent<Rigidbody>().useGravity = false;
+        gameObject.transform.rotation = Quaternion.identity;
+
+        //if object held is out of "arms reach"
+        if (Vector3.Distance(transform.position, hands.transform.position) > hands.GetComponent<PickUpForHands>().outOfLosDist)
+        {
+            hands.GetComponent<PickUpForHands>().carrying = false;
+            InHand = false;
+        }
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        //stops phasing through objects if "hands" are too far away
+        followSpeed = defaultSpeed/10;
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        followSpeed = defaultSpeed;
+    }
+}
