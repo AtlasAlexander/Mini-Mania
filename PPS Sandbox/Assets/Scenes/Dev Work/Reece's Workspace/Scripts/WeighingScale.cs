6af82d35eb94592a3bec2fa210b87e58d3ea1de2
Reece <reecebellchambers98@gmail.com>
2024-01-31 01:21:46 +0000

    Weighing scale tutorial level WIP
    Added the new player controller to test the level out.

--- PPS Sandbox/Assets/Scenes/Dev Work/Reece's Workspace/Scripts/WeighingScale.cs
+++ PPS Sandbox/Assets/Scenes/Dev Work/Reece's Workspace/Scripts/WeighingScale.cs
@@ -15,7 +15,7 @@ public class WeighingScale : MonoBehaviour
 
     private void Awake()
     {
-        playerStats = GameObject.Find("Player").GetComponent<Stats>();
+        playerStats = GameObject.Find("JakePlayer").GetComponent<Stats>();
         //cube1Stats = GameObject.Find("CarryCube").GetComponent<Stats>();
         cube1Stats = cube1.GetComponent<Stats>();
         //cube2Stats = GameObject.Find("CarryCube (1)").GetComponent<Stats>();
