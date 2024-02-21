552898d9c36d98411634410616d0722ce64341ff
Ben Neale <ben0neale@gmail.com>
2024-02-21 16:52:28 +0000

    Level Select Setup done

---
+++ PPS Sandbox/Assets/LevelSelectController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelSelectController : MonoBehaviour
+{
+    public List<Button> buttons;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!PlayerPrefs.HasKey("Level"))
+            PlayerPrefs.SetInt("Level", 1);
+
+        //Set starting button colour
+        if (buttons.Count > 0)
+        {
+            for (int i = 0; i < PlayerPrefs.GetInt("Level"); i++)
+            {
+                buttons[i].image.color = Color.white;
+            }
+
+            for (int i = PlayerPrefs.GetInt("Level"); i < buttons.Count; i++)
+            {
+                buttons[i].image.color = Color.grey;
+                buttons[i].interactable = false;
+            }
+        }
+    }
+
+    public void LoadLevel(int num)
+    {
+        SceneManager.LoadScene(num);
+    }
+}
