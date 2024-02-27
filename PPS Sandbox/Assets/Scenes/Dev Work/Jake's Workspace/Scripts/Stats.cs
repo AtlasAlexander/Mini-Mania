80cd6d519011c338076f48a88c121bd37dce457c
Jake <jake2.webster@live.uwe.ac.uk>
2024-02-27 19:04:37 +0000

    lasers LVL 5 visuals
    Plus change to stats script for concistency

--- PPS Sandbox/Assets/Scenes/Dev Work/Jake's Workspace/Scripts/Stats.cs
+++ PPS Sandbox/Assets/Scenes/Dev Work/Jake's Workspace/Scripts/Stats.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class Stats : MonoBehaviour
 {
-    public float Weight = 100;
-    float DefaultWeight = 100;
+    public float Weight = 50;
+    public float DefaultWeight = 50;
 
     private void Awake()
     {
         DefaultWeight = Weight;
         if(GetComponent<SizeChange>().startSmall)
         {
-            DefaultWeight = Weight * 5;
+            DefaultWeight = 50;
         }
     }
     private void LateUpdate()
