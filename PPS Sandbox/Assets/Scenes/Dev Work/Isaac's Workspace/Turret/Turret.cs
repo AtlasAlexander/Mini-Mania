1d2135a43dcdeb958c94bb7a08a637b0192d5a13
Isaac <isaac2.turpin@live.uwe.ac.uk>
2024-03-11 16:07:19 +0000

    Added shrinkable turrets + new level & last level changes
    new level and edit last level

--- PPS Sandbox/Assets/Scenes/Dev Work/Isaac's Workspace/Turret/Turret.cs
+++ PPS Sandbox/Assets/Scenes/Dev Work/Isaac's Workspace/Turret/Turret.cs
@@ -7,17 +7,51 @@ public class Turret : MonoBehaviour
     [SerializeField] Transform weapon;
     [SerializeField] ParticleSystem projectileParticles;
     [SerializeField] float range = 15f;
+    [SerializeField] GameObject turretBigBolts;
+    [SerializeField] GameObject turretSmallBolts;
     Transform target;
+    SizeChange sizeChange;
 
     private void Start()
     {
         target = FindObjectOfType<FirstPersonController>().transform;
+        sizeChange = GetComponent<SizeChange>();
     }
 
 
     private void Update()
     {
         AimWeapon();
+
+        if(sizeChange != null)
+        {
+            if(sizeChange.GetShrunkStatus())
+            {
+                if (turretSmallBolts != null)
+                {
+                    turretSmallBolts.SetActive(true);
+
+                    if(turretBigBolts != null)
+                    {
+                        turretBigBolts.SetActive(false);
+                    }
+                }
+            }
+
+            if(!sizeChange.GetShrunkStatus())
+            {
+                if(turretBigBolts != null)
+                {
+                    turretBigBolts.SetActive(true);
+
+                    if(turretSmallBolts != null)
+                    {
+                        turretSmallBolts.SetActive(false);
+                    }
+                }
+            }
+        }
+
     }
 
     void AimWeapon()
