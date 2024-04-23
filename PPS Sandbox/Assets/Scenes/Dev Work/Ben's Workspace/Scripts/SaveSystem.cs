cf7381ce0fdf063e227b2182201933b181bb1287
Ben Neale <ben2.neale@live.uwe.ac.uk>
2024-04-23 12:04:59 +0100

    Save system for Pause menu settings - kinda working

--- PPS Sandbox/Assets/Scenes/Dev Work/Ben's Workspace/Scripts/SaveSystem.cs
+++ PPS Sandbox/Assets/Scenes/Dev Work/Ben's Workspace/Scripts/SaveSystem.cs
@@ -8,14 +8,13 @@ public class SaveSystem : MonoBehaviour
     [SerializeField] GameObject PauseOptionMenu;
     GameObject CheckpointsOBJ;
 
+    Slider MasterVolumeSlider;
     Slider MusicSlider;
     Slider SFXSlider;
     Slider XControllersensSlider;
     Slider YControllersensSlider;
     Slider XMousesensSlider;
     Slider YMousesensSlider;
-    Toggle MusicMuteToggle;
-    Toggle SFXMuteToggle;
     Toggle InvertLookToggle;
 
 
@@ -27,55 +26,39 @@ public class SaveSystem : MonoBehaviour
     {
         CheckpointsOBJ = GameObject.Find("CheckpointController");
 
+        MasterVolumeSlider = PauseOptionMenu.transform.Find("Master VolumeSlider").GetComponent<Slider>();
         MusicSlider = PauseOptionMenu.transform.Find("Music VolumeSlider").GetComponent<Slider>();
         SFXSlider = PauseOptionMenu.transform.Find("SFX VolumeSlider").GetComponent<Slider>();
         XControllersensSlider = PauseOptionMenu.transform.Find("XSens Slider - Controller").GetComponent<Slider>();
         YControllersensSlider = PauseOptionMenu.transform.Find("YSens Slider - Controller").GetComponent<Slider>();
         XMousesensSlider = PauseOptionMenu.transform.Find("XSens Slider").GetComponent<Slider>();
         YMousesensSlider = PauseOptionMenu.transform.Find("YSens Slider - Controller").GetComponent<Slider>();
-        MusicMuteToggle = PauseOptionMenu.transform.Find("MusicToggle").GetComponent<Toggle>();
-        SFXMuteToggle = PauseOptionMenu.transform.Find("SFX Toggle").GetComponent<Toggle>();
         InvertLookToggle = PauseOptionMenu.transform.Find("InvertToggle").GetComponent<Toggle>();
 
         if (PlayerPrefs.HasKey("MusicVolume"))
             MusicSlider.value = PlayerPrefs.GetFloat("MusicVolume");
         if (PlayerPrefs.HasKey("SFXVolume"))
             SFXSlider.value = PlayerPrefs.GetFloat("SFXVolume");
+        if (PlayerPrefs.HasKey("MasterVolume"))
+            MasterVolumeSlider.value = PlayerPrefs.GetFloat("MasterVolume");
 
-/*        if (PlayerPrefs.HasKey("XMouseSensitivity"))
-            XMousesensSlider.value = PlayerPrefs.GetFloat("XMouseSensitivity");
-        if (PlayerPrefs.HasKey("YMouseSensitivity"))
-            YMousesensSlider.value = PlayerPrefs.GetFloat("YMouseSensitivity");
-        if (PlayerPrefs.HasKey("XControllerSensitivity"))
-            XControllersensSlider.value = PlayerPrefs.GetFloat("XControllerSensitivity");
-        if (PlayerPrefs.HasKey("YControllerSensitivity"))
-            YControllersensSlider.value = PlayerPrefs.GetFloat("YControllerSensitivity");*/
+        /*        if (PlayerPrefs.HasKey("XMouseSensitivity"))
+                    XMousesensSlider.value = PlayerPrefs.GetFloat("XMouseSensitivity");
+                if (PlayerPrefs.HasKey("YMouseSensitivity"))
+                    YMousesensSlider.value = PlayerPrefs.GetFloat("YMouseSensitivity");
+                if (PlayerPrefs.HasKey("XControllerSensitivity"))
+                    XControllersensSlider.value = PlayerPrefs.GetFloat("XControllerSensitivity");
+                if (PlayerPrefs.HasKey("YControllerSensitivity"))
+                    YControllersensSlider.value = PlayerPrefs.GetFloat("YControllerSensitivity");*/
 
-/*        if (PlayerPrefs.HasKey("InvertLook"))
-        {
-            if (PlayerPrefs.GetInt("InvertLook") == 1)
-                invertLook = true;
-            else
-                invertLook = false;
-            InvertLookToggle.isOn = invertLook;
-        }*/
-
-        if (PlayerPrefs.HasKey("MusicMute"))
-        {
-            if (PlayerPrefs.GetInt("MusicMute") == 1)
-                MusicMuteToggle.isOn = true;
-            else
-                MusicMuteToggle.isOn = false;
-        }
-        if (PlayerPrefs.HasKey("SFXMute"))
-        {
-            if (PlayerPrefs.GetInt("SFXMute") == 1)
-                SFXMute = true;
-            else
-                SFXMute = false;
-            SFXMuteToggle.isOn = SFXMute;
-        }
-
+        /*        if (PlayerPrefs.HasKey("InvertLook"))
+                {
+                    if (PlayerPrefs.GetInt("InvertLook") == 1)
+                        invertLook = true;
+                    else
+                        invertLook = false;
+                    InvertLookToggle.isOn = invertLook;
+                }*/
 
         //checkpoints saves
         if (PlayerPrefs.HasKey("Checkpoint"))
@@ -94,6 +77,11 @@ public class SaveSystem : MonoBehaviour
     public void SaveMusicVolume(float value)
     {
         PlayerPrefs.SetFloat("MusicVolume", value);
+    }
+
+    public void SaveMasterVolume(float value)
+    {
+        PlayerPrefs.SetFloat("MasterVolume", value);
     }
 
     public void SaveSFXVolume(float value)
